2899361070e997877294fd5d191ba8511bfdba43
Gabriel C <dokkillo@users.noreply.github.com>
2017-09-21 07:09:53 +0200

    Return Right HTTP codes

--- WebApi/Controllers/FlightController.cs
+++ WebApi/Controllers/FlightController.cs
@@ -17,64 +17,76 @@ namespace WebApi.Controllers
 
         public IHttpActionResult Get()
         {
-            int passengers;
-            string origin;
-            string destination;
-            DateTime dateIn;
-            DateTime dateOut;
-            bool roundtrip;
-            var db = DataBase.Instance;
-            var flightSearch = new FlightSearch(db);
-            NameValueCollection queryString = HttpUtility.ParseQueryString(Request.RequestUri.Query);
+            try
+            {
+                int passengers;
+                string origin;
+                string destination;
+                DateTime dateIn;
+                DateTime dateOut;
+                bool roundtrip;
+                var db = DataBase.Instance;
+                var flightSearch = new FlightSearch(db);
+                NameValueCollection queryString = HttpUtility.ParseQueryString(Request.RequestUri.Query);
 
-            if(queryString.Get("passengers") == null || queryString.Get("origin") == null  ||
-                queryString.Get("destination") == null || queryString.Get("dateIn") == null ||
-                queryString.Get("roundTrip") == null)
-            {
-                return BadRequest();
-            }
-
-            if(!Int32.TryParse(queryString.Get("passengers"),out passengers))
-            {
-                return BadRequest("passengers must be a number");
-            }
-            if(passengers < 0)
-            {
-                return BadRequest("passengers must be one or more");
-            }
-
-            if(!DateTime.TryParse(queryString.Get("dateIn"),out dateIn))
-            {
-                return BadRequest("dateIn must be a date");
-            }
-
-            if(!Boolean.TryParse(queryString.Get("roundTrip"),out roundtrip))
-            {
-                return BadRequest("Roundtrip parameter is not set to true or false");
-            }
-          
-            //DateOut parameter is optional, only is required if roundtrip is true.
-            if(roundtrip)
-            {
-                if(queryString.Get("dateOut") == null)
+                if (queryString.Get("passengers") == null || queryString.Get("origin") == null ||
+                    queryString.Get("destination") == null || queryString.Get("dateIn") == null ||
+                    queryString.Get("roundTrip") == null)
                 {
                     return BadRequest();
                 }
 
-                if (!DateTime.TryParse(queryString.Get("dateOut"), out dateOut))
+                if (!Int32.TryParse(queryString.Get("passengers"), out passengers))
                 {
-                    return BadRequest("dateOut must be a date");
+                    return BadRequest("passengers must be a number");
+                }
+                if (passengers < 0)
+                {
+                    return BadRequest("passengers must be one or more");
                 }
 
+                if (!DateTime.TryParse(queryString.Get("dateIn"), out dateIn))
+                {
+                    return BadRequest("dateIn must be a date");
+                }
+
+                if (!Boolean.TryParse(queryString.Get("roundTrip"), out roundtrip))
+                {
+                    return BadRequest("Roundtrip parameter is not set to true or false");
+                }
+
+                //DateOut parameter is optional, only is required if roundtrip is true.
+                if (roundtrip)
+                {
+                    if (queryString.Get("dateOut") == null)
+                    {
+                        return BadRequest();
+                    }
+
+                    if (!DateTime.TryParse(queryString.Get("dateOut"), out dateOut))
+                    {
+                        return BadRequest("dateOut must be a date");
+                    }
+
+                }
+
+                origin = queryString.Get("origin");
+                destination = queryString.Get("destination");
+
+                var result = flightSearch.Search(origin, destination, passengers, dateIn, DateTime.MinValue, false);
+
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
             }
-
-            origin = queryString.Get("origin");
-            destination = queryString.Get("destination");
-
-            var result = flightSearch.Search(origin, destination, passengers, dateIn, DateTime.MinValue, false);            
-
-
-            return Ok(result);
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError);
+                throw;
+            }
         }
 
 
