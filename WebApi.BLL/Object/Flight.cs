ad94f359cb5249d7f39f758371f8f89754937fb3
Gabriel C <dokkillo@users.noreply.github.com>
2017-09-21 23:36:46 +0200

    add xml formater

--- WebApi.BLL/Object/Flight.cs
+++ WebApi.BLL/Object/Flight.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace WebApi.BLL.Object
 {
+    [DataContract]
     public class Flight
     {
+        [DataMember]
         public string Key { get; set; }
+
+        [DataMember]
         public string Origin { get; set; }
+
+        [DataMember]
         public string Destintation { get; set; }
+
+        [DataMember]
         public DateTime Time { get; set; }
 
         public Flight(string _Key, string _Origin, string _Destination, DateTime _Time)
