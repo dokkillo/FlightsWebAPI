8bbc3e42da90f34937976f14c1483c1007ef90f3
Gabriel C <dokkillo@users.noreply.github.com>
2017-09-26 07:43:46 +0200

    New interface Irepository

---
+++ WebApi.DB/Repository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace WebApi.DB.Repository
+{
+    public interface IRepository<T>
+    {
+        void Add(T newEntity);
+        void Remove(T entity);
+        IQueryable<T> Find(Expression<Func<T, bool>> predicate);
+
+    }
+}
